dc42fcf3dd68d003cd3095cd6be3da7bb70212c1
Daniel Gould <daniel@kaizenclouds.net>
2023-04-09 14:20:44 +0100

    Update Sofar2mqtt.ino
    Move to cloud-based MQTT with TLS and dynsec

--- Sofar2mqtt/Sofar2mqtt.ino
+++ Sofar2mqtt/Sofar2mqtt.ino
@@ -1,3 +1,37 @@
+static const char ca_cert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
 // Update these to match your inverter/network.
 #define INVERTER_ME3000				// Uncomment for ME3000
 //#define INVERTER_HYBRID			// Uncomment for Hybrid
@@ -8,10 +42,12 @@ const char* version = "v2.1.1";
 
 #define WIFI_SSID	"guest23"
 #define WIFI_PASSWORD	"747-jumbo-jet"
-#define MQTT_SERVER	"192.168.0.15"
-#define MQTT_PORT	1883
+#define MQTT_SERVER	"iok-dev.iok.cloud"
+#define MQTT_PORT	8883
 #define MQTT_USERNAME	"sofar"			// Empty string for none.
-#define MQTT_PASSWORD	"Sierr4Foxtr0t"
+#define MQTT_PASSWORD	"sofar"
+
+
 
 /*****
 Sofar2mqtt is a remote control interface for Sofar solar and battery inverters.
@@ -87,7 +123,7 @@ calcCRC by angelo.compagnucci@gmail.com and jpmzometa@gmail.com
 #error You must specify the inverter type.
 #endif
 
-#include <Arduino.h>
+// #include <Arduino.h>
 
 #define SOFAR_SLAVE_ID          0x01
 
@@ -104,12 +140,21 @@ const String macaddr = WiFi.macAddress();
 const char* deviceName = macaddr.c_str();
 const char* mqttClientID = deviceName;
 const char* wifiName = WIFI_SSID;
-WiFiClient wifi;
+
+BearSSL::WiFiClientSecure wifi;
+// WiFiClient wifi;
+
+
 
 // MQTT parameters
 #include <PubSubClient.h>
 
+
+
 PubSubClient mqtt(wifi);
+
+
+
 
 // SoftwareSerial is used to create a second serial port, which will be deidcated to RS485.
 // The built-in serial port remains available for flashing and debugging.
@@ -250,6 +295,25 @@ struct modbusResponse
 #include <Adafruit_SSD1306.h>
 #define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
+
+void setClock()
+{
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
+
 
 /**
  * Check to see if the elapsed interval has passed since the passed in
@@ -908,8 +972,11 @@ void setup()
 	display.display();
 	updateOLED(deviceName, "connecting", "", version);
 
-	setup_wifi();
+  BearSSL::X509List *serverTrustedCA = new BearSSL::X509List(ca_cert);
+  wifi.setTrustAnchors(serverTrustedCA);
 
+	setup_wifi();
+  setClock();
 	mqtt.setServer(MQTT_SERVER, MQTT_PORT);
 	mqtt.setCallback(mqttCallback);
 
